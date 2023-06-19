1086fb9e16e080609e984fbdd5e8d8750b0b503d
Ian Allish <ianallish@Ians-MBP.lan>
2023-06-19 00:20:20 -0400

    Added and stylized dropdown menu to home page.

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -1,7 +1,8 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Menu } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 export default function Home() {
@@ -24,7 +25,7 @@ export default function Home() {
             >
               <img
                 src="/home/portfolio.jpg"
-                className="flex min-h-48 px-16 max-h-[50vh] rounded-md"
+                className="flex min-h-48 px-16 max-h-[60vh] rounded-md"
               />
             </motion.div>
             <div className="flex flex-col justify-center max-w-[60vw]">
@@ -46,32 +47,76 @@ export default function Home() {
                   Currently strengthening skills related to HTML/CSS/JS and C++.
                 </div>
               </motion.div>
-              <div className="text-xl">It iss a pleasure to meet you.</div>
-              <div>
-                <Menu as="div" className="relative inline-block">
-                  <div>
-                    <Menu.Button className="inline-flex w-full justify-center rounded-md bg-white bg-opacity-20 px-4 py-2 text-lg font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                      Let me introduce myself.
-                      <ChevronDownIcon
-                        className="ml-2 -mr-1 h-7 w-87 text-white align-center"
-                        aria-hidden="true"
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Menu.Items>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          className={`${active && "bg-blue-500"}`}
-                          href="/account-settings"
-                        >
-                          Inspirations
-                        </a>
-                      )}
-                    </Menu.Item>
-                  </Menu.Items>
-                </Menu>
-              </div>
+              <motion.div
+                initial={{ y: 25, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ delay: 2.5, duration: 0.75 }}
+              >
+                <div>
+                  <Menu as="div" className="relative inline-block">
+                    <div>
+                      <Menu.Button className="inline-flex w-full justify-center rounded-md bg-neutral-700 px-4 py-2 text-lg font-medium text-white hover:bg-neutral-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                        It is a pleasure to meet you. Let me introduce myself.
+                        <ChevronDownIcon
+                          className="ml-2 -mr-1 h-7 w-87 text-white align-center"
+                          aria-hidden="true"
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 mt-2 w-full origin-top-right divide-y divide-neutral-700 rounded-md bg-neutral-700 ring-1 ring-neutral-700 ring-opacity-2 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={`${
+                                active
+                                  ? "bg-neutral-600 text-white"
+                                  : "text-white bg-neutral-700"
+                              } group flex w-full items-center rounded-md px-2 py-2 text-md`}
+                            >
+                              Inspiration
+                            </button>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={`${
+                                active
+                                  ? "bg-neutral-600 text-white"
+                                  : "text-white bg-neutral-700"
+                              } group flex w-full items-center rounded-md px-2 py-2 text-md`}
+                            >
+                              Projects
+                            </button>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={`${
+                                active
+                                  ? "bg-neutral-600 text-white"
+                                  : "text-white bg-neutral-700"
+                              } group flex w-full items-center rounded-md px-2 py-2 text-md`}
+                            >
+                              Resume
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              </motion.div>
             </div>
           </div>
         </div>
