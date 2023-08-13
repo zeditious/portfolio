9c7722f185ef5b8a136e533b39ccd1b34f8b6aea
Ian Allish <ianallish@Ians-MBP.lan>
2023-08-13 02:26:36 -0400

    Added sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://acme.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://acme.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://acme.com/blog",
+      lastModified: new Date(),
+    },
+  ];
+}
